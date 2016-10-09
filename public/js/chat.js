d2f6abe191795d4737598fb5504ecb6205208255
Adrien Folie <folie.adrien@gmail.com>
2016-10-10 00:54:54 +0200

    remove code smells in client

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,31 +1,17 @@
 (function(fn){var d=document;(d.readyState=='loading')?d.addEventListener('DOMContentLoaded',fn):fn();})(function(){
   var selectors = {
     button: {
-      join: function() {
-        return document.getElementById('join-room');
-      },
-      send: function() {
-        return document.getElementById('send');
-      }
+      join: document.getElementById('join-room'),
+      send: document.getElementById('send')
     },
     input: {
-      room: function() {
-        return document.querySelector('input[name="room"]');
-      },
-      message: function() {
-        return document.querySelector('input[name="message"]');
-      }
+      room:    document.querySelector('input[name="room"]'),
+      message: document.querySelector('input[name="message"]')
     },
     block: {
-      messages: function() {
-        return document.getElementById('messages');
-      },
-      members: function() {
-        return document.getElementById('members');
-      },
-      activeRoom: function() {
-        return document.getElementById('active-room');
-      }
+      messages:   document.getElementById('messages'),
+      members:    document.getElementById('members'),
+      activeRoom: document.getElementById('active-room')
     }
   };
 
@@ -41,50 +27,50 @@
   UI.prototype.initialize = function() {
     var self = this;
 
-    this.selectors.button.join().disabled = true;
-    this.selectors.button.send().disabled = true;
+    this.selectors.button.join.disabled = true;
+    this.selectors.button.send.disabled = true;
 
-    this.selectors.input.room().focus();
+    this.selectors.input.room.focus();
 
-    this.selectors.button.join().addEventListener('click', function() {
-      self.activeRoom = self.selectors.input.room().value;
+    this.selectors.button.join.addEventListener('click', function() {
+      self.activeRoom = self.selectors.input.room.value;
 
       self.client.emit('join-room', self.activeRoom);
     });
 
-    this.selectors.button.send().addEventListener('click', function() {
-      var message = self.selectors.input.message().value,
-        roomName  = self.selectors.block.activeRoom().innerHTML;
+    this.selectors.button.send.addEventListener('click', function() {
+      var message = self.selectors.input.message.value,
+        roomName  = self.selectors.block.activeRoom.innerHTML;
 
       self.client.emit('message', message);
     });
 
-    this.selectors.input.room().addEventListener('input', function(input) {
-      if (self.selectors.input.room().value == '') {
-        self.selectors.button.join().disabled = true;
+    this.selectors.input.room.addEventListener('input', function(input) {
+      if (self.selectors.input.room.value == '') {
+        self.selectors.button.join.disabled = true;
       } else {
-        self.selectors.button.join().disabled = false;
+        self.selectors.button.join.disabled = false;
       }
     });
 
-    this.selectors.input.room().addEventListener('keydown', function(e) {
+    this.selectors.input.room.addEventListener('keydown', function(e) {
       if (e.which !== 13) { return; }
 
-      self.selectors.button.join().click();
+      self.selectors.button.join.click();
     });
 
-    this.selectors.input.message().addEventListener('input', function(input) {
-      if (!self.isInARoom || self.selectors.input.message().value.trim() == '') {
-        self.selectors.button.send().disabled = true;
+    this.selectors.input.message.addEventListener('input', function(input) {
+      if (!self.isInARoom || self.selectors.input.message.value.trim() == '') {
+        self.selectors.button.send.disabled = true;
       } else {
-        self.selectors.button.send().disabled = false;
+        self.selectors.button.send.disabled = false;
       }
     });
 
-    this.selectors.input.message().addEventListener('keydown', function(e) {
+    this.selectors.input.message.addEventListener('keydown', function(e) {
       if (e.which !== 13) { return; }
 
-      self.selectors.button.send().click();
+      self.selectors.button.send.click();
     });
 
     this.client.on('connect', function() {
@@ -95,14 +81,14 @@
       self.activeRoom = false;
       self.inARoom    = false;
 
-      self.selectors.button.join().disabled = true;
-      self.selectors.button.send().disabled = true;
+      self.selectors.button.join.disabled = true;
+      self.selectors.button.send.disabled = true;
 
-      self.selectors.block.activeRoom().innerHTML = 'Waiting for reconnection...';
+      self.selectors.block.activeRoom.innerHTML = 'Waiting for reconnection...';
     });
 
     this.client.on('reconnect', function() {
-      self.selectors.block.activeRoom().innerHTML = 'No active channel';
+      self.selectors.block.activeRoom.innerHTML = 'No active channel';
     });
 
     this.client.on('room-infos', function(room) {
@@ -126,27 +112,27 @@
   };
 
   UI.prototype.activate = function() {
-    this.selectors.input.room().disabled = false;
+    this.selectors.input.room.disabled = false;
 
-    this.selectors.input.room();
+    this.selectors.input.room;
   };
 
   UI.prototype.populateChat = function(content) {
-    this.selectors.input.message().value = '';
+    this.selectors.input.message.value = '';
 
-    this.selectors.button.send().disabled = true;
+    this.selectors.button.send.disabled = true;
 
-    this.selectors.block.messages().innerHTML += '<code>[' + content.sentAt + '] [' + content.sender + '] ' + content.text + '</code><br>';
+    this.selectors.block.messages.innerHTML += '<code>[' + content.sentAt + '] [' + content.sender + '] ' + content.text + '</code><br>';
   };
 
   UI.prototype.loadRoom = function(room) {
-    this.selectors.block.messages().innerHTML = '';
+    this.selectors.block.messages.innerHTML = '';
 
-    this.selectors.block.activeRoom().innerHTML = '#' + this.activeRoom;
+    this.selectors.block.activeRoom.innerHTML = '#' + this.activeRoom;
 
     this.isInARoom = true;
 
-    this.selectors.block.members().innerHTML = '';
+    this.selectors.block.members.innerHTML = '';
 
     var self = this;
 
@@ -154,13 +140,13 @@
       self.addMember(nickname);
     });
 
-    this.selectors.input.message().focus();
+    this.selectors.input.message.focus();
 
-    this.selectors.block.messages().innerHTML += '<code>** Welcome to #' + this.activeRoom + '. **</code><br>';
+    this.selectors.block.messages.innerHTML += '<code>** Welcome to #' + this.activeRoom + '. **</code><br>';
   };
 
   UI.prototype.addMember = function(nickname) {
-    this.selectors.block.members().innerHTML += '<li id="' + nickname + '" class="list-group-item">' + nickname + '</li>';
+    this.selectors.block.members.innerHTML += '<li id="' + nickname + '" class="list-group-item">' + nickname + '</li>';
   };
 
   UI.prototype.removeMember = function(nickname) {
